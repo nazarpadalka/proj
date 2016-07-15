1988fd97c702fba2d1ac16fa478b6f5cc576617b
unknown <tcygankov.a.v@gmail.com>
2016-07-15 17:27:33 +0300

    zadacha

--- lingmap.php
+++ lingmap.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$str = '';
+
 $str =  get_string();
 $PreCrash =  string_to_arr($str);
 $mapped = mapping($PreCrash);
@@ -9,8 +9,10 @@ $sorted = sorting($mapped);
 $unmapped = unmap($sorted);
 
 
+
+
 function get_string () {
-    $str = "6B,10E,AC,QB,1B,QE,QC";
+    $str = "6B,6E,AC,QB,1B,QE,QC,JC,2E,4B";
     return $str;
 }
 
@@ -25,42 +27,24 @@ function crash_arr ($PreCrash){
         $m = $str[strlen($str) - 1];
         $str = str_replace($m, '', $str);
         $Crashed[] = array("m" => "$m", "d" => $str);
-        // var_dump($nominal);
          }
     return $Crashed;
     }
 
 function mapping($PreCrash){
-
-  //  var_dump($PreCrash);
-    //$MappingNumbersNom = array('1000', '2000', '3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000', '11000', '12000', '13000', '14000');
-    //$CardNominal = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k", "a");
-
-    // $nominal = str_replace(array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'), array('1000', '2000', '3000', '4000', '5000', '6000', '7000', '8000', '9000', '10000', '11000', '12000', '13000', '14000'), $PreCrash);
     $PreCrash = str_replace(array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'), array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'), $PreCrash);
-
-  //  var_dump($PreCrash);
     return ($PreCrash);
 }
 
 function unmap($sorted){
-
     $unmapped = str_replace(array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'), array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'), $sorted);
-
-      var_dump($unmapped);
     return ($unmapped);
 }
 
-
-
 function sorting($mapped){
-   // var_dump($mapped);
-   sort($mapped);
+    sort($mapped);
     $sorted = $mapped;
-
-  //  var_dump($sorted);
     return ($sorted);
-
     }
 ?>
 
