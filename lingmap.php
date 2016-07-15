6f6b56764d81cc04f441cda71177162b6365bfe1
nazarpadalka <padalka.nazar@gmail.com>
2016-07-15 17:47:38 +0300

    Update lingmap.php

--- lingmap.php
+++ lingmap.php
@@ -1,5 +1,5 @@
 <?php
-
+//что за пустые строки?
 
 
 $str =  get_string();
@@ -12,12 +12,12 @@ $unmapped = unmap($sorted);
 
 
 function get_string () {
-    $str = "6B,6E,AC,QB,1B,QE,QC,JC,2E,4B";
+    $str = "6B,6E,AC,QB,1B,QE,QC,JC,2E,4B"; //нахера лишняя переменная?
     return $str;
 }
 
 function string_to_arr ($str){
-    $PreCrash = explode(",", $str);
+    $PreCrash = explode(",", $str);//нахера лишняя переменная?
     return $PreCrash;
     }
 
@@ -27,28 +27,24 @@ function crash_arr ($PreCrash){
         $m = $str[strlen($str) - 1];
         $str = str_replace($m, '', $str);
         $Crashed[] = array("m" => "$m", "d" => $str);
-         }
+         } //что за форматирование?
     return $Crashed;
     }
 
 function mapping($PreCrash){
-    $PreCrash = str_replace(array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'),
-                            array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'),
-                            $PreCrash);
-    return ($PreCrash);
+    $PreCrash = str_replace(array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'), array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'), $PreCrash);
+    return ($PreCrash);//нахера лишняя переменная?
 }
 
 function unmap($sorted){
-    $unmapped = str_replace(array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'),
-                            array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'),
-                            $sorted);
-    return ($unmapped);
+    $unmapped = str_replace(array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'), array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'), $sorted);
+    return ($unmapped);//нахера лишняя переменная?
 }
 
 function sorting($mapped){
     sort($mapped);
-    $sorted = $mapped;
+    $sorted = $mapped;//нахера лишняя переменная?
     return ($sorted);
     }
-?>
-
+?>//нахера закрывающий тэг?
+что за пустые строки?
