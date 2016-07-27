6463dcab89ee3e93a186a439b97c6b853d5dc6c1
unknown <tcygankov.a.v@gmail.com>
2016-07-27 11:53:55 +0300

    oop

--- OOP1.php
+++ OOP1.php
@@ -4,12 +4,10 @@
 
 class getSting {
 
-      var $str;
-
-    function get_string (){
+    public function get_string (){
         return $this->str = "6B,6E,AC,QB,1B,QE,QC,JC,2E,4B";
     }
-    function result(){
+    public function result(){
         echo "< /br> $mapping->precrash< /br>";
     }
 }
@@ -18,13 +16,13 @@ class getSting {
 
 class prepareString {
 
-    var $PreCrash;
-    var $Crashed;
+    public $PreCrash;
+    public $Crashed;
 
-    function string_to_arr ($str){
+    public function string_to_arr ($str){
         return explode(",", $str);
     }
-    function crash_arr ($PreCrash){
+    public function crash_arr ($PreCrash){
         $Crashed = array();
         foreach ($PreCrash as $str) {
             $m = $str[strlen($str) - 1];
@@ -38,15 +36,15 @@ class prepareString {
 
 class mapping {
 
-    var $PreCrash;
-    var $sorted;
+    public  $PreCrash;
+    public  $sorted;
 
-    function mapping ($PreCrash){
+    public function mapping ($PreCrash){
         return  str_replace (array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'),
                              array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'),
                              $PreCrash);
     }
-    function unmapping ($sorted){
+    public function unmapping ($sorted){
         return  str_replace(array('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'a'),
                             array('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', '1'),
                             $sorted);
@@ -55,11 +53,19 @@ class mapping {
 
 class sorting {
 
-    var $sorted;
-    var $mapped;
+    public  $sorted;
+    public  $mapped;
 
-    function sorting ($mapped){
+    public function sorting ($mapped){
         return  sort($mapped);
     }
 }
 ////////////////////////////////////////////////////////////
+/*
+$getString = new getString();
+$prepareString = new prepareString();
+$mapping =new mapping();
+$sorting = new sorting;
+*/
+
+
