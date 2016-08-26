8301a85e323b9434510949843648b1173a1d5ae6
unknown <tcygankov.a.v@gmail.com>
2016-08-26 16:14:03 +0300

    refactoring

--- application/views/viewCart.php
+++ application/views/viewCart.php
@@ -1,8 +1,5 @@
 <?php
 require_once 'application\core\view.php';
-/**
- *Класс для отображения контента страницы с сортировкой карт
- */
 class ViewCart extends View
 {
 	function generate($teplate, $content, $data)
