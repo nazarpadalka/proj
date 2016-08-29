511dbad92384aaf6b1e52719a5e0082402d5a4c8
unknown <tcygankov.a.v@gmail.com>
2016-08-30 00:10:56 +0300

    new menu

--- application/menu.php
+++ application/menu.php
@@ -1,18 +1,15 @@
 <?php
 
-$menu[$i=0]['url']='/';
-$menu[$i]['name']='Main';
+$menu[$position]['url']='/';
+$menu[$position]['name']='Main';
 
-$menu[++$i]['url']='index.php?act=Cart';
-$menu[$i]['name']='Carts';
-var_dump($menu);
+$menu[++$position]['url']='index.php?act=Cart';
+$menu[$position]['name']='Carts';
 
 echo "<ul>\n";
 foreach($menu as $url)
 {
     echo ($_SERVER["REQUEST_URI"] = $url['url']) ? '<li class="active">': '<li>';
-    echo "<a href=\"".$url['url']."\">".$url['name']."</a></li>\n";
+    echo '<a href="' . $url['url'] . '">' . $url['name'] . "</a></li>\n";
 }
 echo "</ul>";
-
-?>
