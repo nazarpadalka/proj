1e279c1ecb2a93464791225f768c444dc89f00f4
nazarpadalka <padalka.nazar@gmail.com>
2016-08-31 15:50:41 +0300

    Update controllers_controllerCart.php

--- application/controllers/controllers_controllerCart.php
+++ application/controllers/controllers_controllerCart.php
@@ -10,18 +10,20 @@ class controllers_controllerCart extends core_controller
      */
 	function __construct()
 	{
+		/** doc-block for each variable */ 
 		$this->model = new models_ModelCart();
 		$this->view = new views_ViewCart();
 		$this->validator = new validators_validatorCart();
 	}
 
 	/**
-	 *
+	 * empty doc
      */
 	function actionIndex()
 	{
-		$input = $this->validator->Check();
-		if ($input == true){
+		$input = $this->validator->Check(); //not nessesary
+		//declarate $data here
+		if ($input == true){ //just if ($this->validator->Check())
 			$data = 'Error';
 		}
 		else{
